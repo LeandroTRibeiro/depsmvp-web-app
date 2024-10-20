0e4a1d664444b287c63ba352fd9c340a779600c6
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-20 08:45:26 -0300

    feat: declare not-found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,16 @@ import { HomeComponent } from './components/pages/home/home.component';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatListModule} from "@angular/material/list";
 import {MatIconModule} from "@angular/material/icon";
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
     AppComponent,
     HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
