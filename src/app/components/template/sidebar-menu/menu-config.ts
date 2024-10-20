5d29932e8c8a75c9fec2b3ccee15ede3286e9e72
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-20 18:34:26 -0300

    refactor: rename consultation route and name

--- src/app/components/template/sidebar-menu/menu-config.ts
+++ src/app/components/template/sidebar-menu/menu-config.ts
@@ -2,7 +2,7 @@
   { key: 'sidebar_menu.administrator', route: '/administrator', icon: 'person' },
   { key: 'sidebar_menu.dashboard', route: '/dashboard', icon: 'dashboard' },
   { key: 'sidebar_menu.credit_policy', route: '/credit-policy', icon: 'credit_card' },
-  { key: 'sidebar_menu.consults', route: '/consults', icon: 'search' },
+  { key: 'sidebar_menu.consultations', route: '/consultations', icon: 'search' },
   { key: 'sidebar_menu.invoices', route: '/invoices', icon: 'receipt' },
   { key: 'sidebar_menu.account', route: '/account', icon: 'account_balance' },
   { key: 'sidebar_menu.messages', route: '/messages', icon: 'message' },
