9c2e30d824bc423057e2032a84bf6d0c90855899
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-20 08:37:41 -0300

    feat: add sidebar-menu

---
+++ src/app/components/template/sidebar-menu/menu-config.ts
@@ -0,0 +1,12 @@
+﻿export const MENU_CONFIG = [
+  { key: 'sidebar_menu.administrator', route: '/administrator', icon: 'person' },
+  { key: 'sidebar_menu.dashboard', route: '/dashboard', icon: 'dashboard' },
+  { key: 'sidebar_menu.credit_policy', route: '/credit-policy', icon: 'credit_card' },
+  { key: 'sidebar_menu.consults', route: '/consults', icon: 'search' },
+  { key: 'sidebar_menu.invoices', route: '/invoices', icon: 'receipt' },
+  { key: 'sidebar_menu.account', route: '/account', icon: 'account_balance' },
+  { key: 'sidebar_menu.messages', route: '/messages', icon: 'message' },
+  { key: 'sidebar_menu.administration', route: '/administration', icon: 'settings' },
+  { key: 'sidebar_menu.help', route: '/help', icon: 'help' },
+  { key: 'sidebar_menu.logout', route: '/logout', icon: 'logout' }
+];
---
+++ src/app/components/template/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {MENU_CONFIG} from "./menu-config";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {ResponsiveSidebarMenuService} from "../../../shared/services/responsive-sidebar-menu.service";
+
+@Component({
+  selector: 'app-sidebar-menu',
+  templateUrl: './sidebar-menu.component.html',
+  styleUrls: ['./sidebar-menu.component.scss']
+})
+export class SidebarMenuComponent implements OnInit {
+  menuItens = MENU_CONFIG;
+  isMenuOpened = true;
+  isHandSet = true;
+
+  constructor(
+    private _breakpointerOberver: BreakpointObserver,
+    private _responsiveSidebarMenuService: ResponsiveSidebarMenuService,
+  ) {}
+
+  handlerIsMenuOpen(){
+    this._responsiveSidebarMenuService.toogleMenu();
+  };
+
+  ngOnInit(): void {
+    this._breakpointerOberver.observe([Breakpoints.Handset]).subscribe( result => {
+      this.isHandSet = result.matches;
+      this._responsiveSidebarMenuService.setMenuState(!result.matches);
+    });
+
+    this._responsiveSidebarMenuService.isMenuOpen$.subscribe(isMenuOpen => {
+      this.isMenuOpened = isMenuOpen;
+    });
+  }
+}
