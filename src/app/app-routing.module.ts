4858b1c42a5714a44050903a15ffea0587e56779
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-20 08:40:25 -0300

    feat: add home and not-found routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/pages/home/home.component";
+import {NotFoundComponent} from "./components/pages/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
