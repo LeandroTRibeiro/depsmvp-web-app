09bc0ac47b5547b604a68c3c35fcd58c9bf725ad
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-28 17:06:21 -0300

    feat: add consultationResponse model

---
+++ src/app/models/consultations-response.model.ts
@@ -0,0 +1,11 @@
+import {Consultation} from "./consultation.model";
+
+export interface ConsultationsResponse {
+  data: Consultation[];
+  pageNumber: number;
+  pageSize: number;
+  totalItems: number;
+  totalPages: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+}
