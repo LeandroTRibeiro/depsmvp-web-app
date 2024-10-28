8ab3891b80bf9d2ca822529b9d83a87b83ad9958
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-28 17:05:25 -0300

    feat: add consultation custom interval component

---
+++ src/app/components/dialogs/consultation-custom-interval/consultation-custom-interval.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConsultationCustomIntervalComponent } from './consultation-custom-interval.component';
+
+describe('ConsultationCustomIntervalComponent', () => {
+  let component: ConsultationCustomIntervalComponent;
+  let fixture: ComponentFixture<ConsultationCustomIntervalComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ConsultationCustomIntervalComponent]
+    });
+    fixture = TestBed.createComponent(ConsultationCustomIntervalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/dialogs/consultation-custom-interval/consultation-custom-interval.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultation-custom-interval',
+  templateUrl: './consultation-custom-interval.component.html',
+  styleUrls: ['./consultation-custom-interval.component.scss']
+})
+export class ConsultationCustomIntervalComponent {
+
+}
